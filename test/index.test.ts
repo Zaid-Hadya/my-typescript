1bcdafe8ac086d761d8c389948db67bc47110b6e
Zaid Hadya <zaid.omar962@gmail.com>
2024-06-27 22:47:26 +0400

    Connect to the db using MongoMemoryServer

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,54 +1,114 @@
-import { app, startServer } from "../src/index"; // Ensure this path is correct based on your project structure
+import { app, startServer } from "../src/index";
 import request from 'supertest';
 import mongoose from 'mongoose';
 import { Server } from 'http';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { MovieModel } from "../src/index";
 
 let server: Server;
+let mongod: MongoMemoryServer;
 
-describe("Test the Get Endpoint", () => {
+const testData = [{
+  "_id": "665462a0dd89506b96ae14b7",
+  "title": "movie1",
+  "release_date": "2007-10-15",
+  "description": "movie1",
+  "image_url": "image1"
+}, {
+  "_id": "665462a0dd89506b96ae14b8",
+  "title": "movie2",
+  "release_date": "2001-05-11",
+  "description": "movie2",
+  "image_url": "image2"
+}];
+
+describe("Test the Endpoints", () => {
   beforeAll(async () => {
-    jest.setTimeout(30000); 
-    // Establish connection to the database
-    await mongoose.connect("mongodb://localhost:27017/MovieStore", {
-    
-    });
-    // Start the server on a different port for testing
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    await mongoose.disconnect();
+    await mongoose.connect(uri);
     server = startServer(4001);
   });
 
   afterAll(async () => {
-    // Close the server and the DB connection
-    await server.close();
+    await mongoose.connection.dropDatabase();
     await mongoose.connection.close();
+    await mongod.stop();
+    server.close();
   });
 
-  test("It should give a 200 response.", async () => {
-    const res = await request(app).get('/getMovies').send()
+  afterEach(async () => {
+    await MovieModel.deleteMany({})
+  });
+
+  test("should return empty array when there is no data", async () => {
+    const res = await request(app).get('/getMovies');
     expect(res.statusCode).toBe(200);
+    expect(res.body).toBeDefined();
+    expect(res.body).toHaveLength(0);
+    expect(res.body).toEqual([]);
   });
 
-//Test Get in case of invalid id
-  test("It should give a 400 response.", async () => {
-    const res = await request(app).get('/getMovie/:id').send({
-      _id: 4
+  test("should return array of movies", async () => {
+    await MovieModel.insertMany(testData);
+    const res = await request(app).get('/getMovies');
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBeDefined();
+    expect(res.body).toHaveLength(2);
+    expect(res.body).toEqual(testData);
+  });
+
+  test("should return movie for given id", async () => {
+    await MovieModel.insertMany(testData);
+    const res = await request(app).get('/getMovie/665462a0dd89506b96ae14b8');
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBeDefined();
+    expect(res.body).toEqual(testData[1]);
+  });
+
+  test("should create a new movie and return status true", async () => {
+    const res = await request(app).post('/createMovie').send({
+      title: "new movie",
+      release_date: "2020-02-02",
+      description: "This is a new movie",
+      image_url: "This is a new movie's URL",
     });
-    expect(res.statusCode).toBe(400);
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toBeDefined();
+    expect(res.body).toEqual({"status":"true"});
+  });
+
+  test("should create a new movie and check movies list", async () => {
+    const res = await request(app).post('/createMovie').send({
+      title: "new movie",
+      release_date: "2020-02-02",
+      description: "This is a new movie",
+      image_url: "This is a new movie's URL",
+    });
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toBeDefined();
+    expect(res.body).toEqual({"status":"true"});
+
+    const getRes = await request(app).get('/getMovies');
+    expect(getRes.statusCode).toBe(200);
+    expect(getRes.body).toBeDefined();
+    expect(getRes.body).toHaveLength(1);
+    expect(getRes.body[0].description).toBe("This is a new movie");
   });
 
 
-  // //Test the Create endpoint with correct info.
-  // test("It should show 201", async () => {
-  //   const res = await request(app).post('/createMovie').send({
-  //     title: "Movie",
-  //     release_date: "2020-02-02",
-  //     description: "This is a movie",
-  //     image_url: "This is a movie's URL",
-  //   });
-  //   expect(res.statusCode).toBe(201);
-  // });
 
-    //Test the Create endpoint with duplicate title.
+  // Test the Create endpoint with duplicate title.
   test("It should show 409", async () => {
+    // First, create the movie
+    await request(app).post('/createMovie').send({
+      title: "Movie",
+      release_date: "2020-02-02",
+      description: "This is a movie",
+      image_url: "This is a movie's URL",
+    });
+    // Then, try to create it again
     const res = await request(app).post('/createMovie').send({
       title: "Movie",
       release_date: "2020-02-02",
@@ -58,20 +118,27 @@ describe("Test the Get Endpoint", () => {
     expect(res.statusCode).toBe(409);
   });
 
-//Test Delete in case of invalid id
-test("It should give a 404 response.", async () => {
-  const res = await request(app).get('/delete/:id').send({
-    _id: 10
-  });
-  expect(res.statusCode).toBe(404);
-});
+  // Test Delete in case of invalid id
+  test("should delete a movie and check movies list", async () => {
+    const res = await request(app).delete('/delete').send({
+      
+      
+    });
+    expect(res.statusCode).toBe(404);
 
-//Test Update in case of invalid id
-test("It should give a 404 response.", async () => {
-  const res = await request(app).get('/update/:id').send({
-    _id: 10
-  });
-  expect(res.statusCode).toBe(404);
-});
+    const getRes = await request(app).get('/getMovies');
+    expect(getRes.statusCode).toBe(200);
+    expect(getRes.body).toBeDefined();
+    
 
+  });
+
+
+  test("It should give a 500 response.", async () => {
+    const res = await request(app).put('/update').send({
+     _id: "665462a0dd89506b96ae14b7",
+      title: "Updated Movie"
+    });
+    expect(res.statusCode).toBe(404);
+  });
 });
